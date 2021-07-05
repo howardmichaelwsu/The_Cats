9b5653192d6248c51b0750c8ceefb2b726730826
Courtney Swanson <Courtney@Courtneys-MacBook-Pro.local>
2021-07-05 01:48:06 -0700

    added to home page and completed registeration

--- cat/cat_app/models.py
+++ cat/cat_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from datetime import datetime
+import bcrypt, re
 
-# Create your models here.
+class UserManager(models.Manager):
+	def basic_validation(self, post_data):
+		#error message, starts as empty tuple adds if anything doesn't match credintials
+		errors = {}
+		#email format validation
+		EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+		#setting length minimum for password, first & last name and error message that pops up directing user of error
+		if len(post_data['password']) < 8:
+			errors['password'] = "Your password needs to be at least 8 characters"
+		if len(post_data['password']) > 60:
+			errors['password'] = "Password can not be over 45 characters"
+		if len(post_data['first_name']) < 2:
+			errors['first_name'] = "First name must be 2 characters or more"
+		if len(post_data['last_name']) < 2:
+			errors['last_name'] ="Last name must be 2 characters or more"
+		#validating email
+		if not EMAIL_REGEX.match(post_data['email']):
+			errors['email'] = "Email must be valid"
+		emailcheck = self.filter(email=post_data['email'])
+		if emailcheck:
+			errors['email'] = 'email already in use'
+		#make sures passwords match in confirm password part of register
+		if post_data['password'] != post_data['confirm_password']:
+			errors['password'] = "Passwords must match"
+		#returns any error in registration, if not will lead to success page
+		return errors 
+
+# Users info
+class User(models.Model):
+	first_name = models.CharField(max_length=45)
+	last_name = models.CharField(max_length=45)
+	email = models.CharField(max_length=45)
+	password = models.CharField(max_length=60)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	# to call to UserManager to help validate info
+	objects =UserManager()
+
+
+	
+		
--- cat/cat_app/views.py
+++ cat/cat_app/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
+from django.db.models import Sum
+import requests
 
 def index(request):
 	return render(request, "index.html")
+
+def register(request):
+	# setting up errors from models.py to actually display on webpage
+	if request.method == 'GET':
+		return redirect('/')
+	errors = User.objects.basic_validation(request.POST)
+	#making sure there isn't an empty field and will print error on homepage(index.html)
+	if len(errors) > 0:
+		for key, value in errors.items():
+			messages.error(request, value)
+		return redirect('/')
+	#hashing/encrypting the password with bcrypt
+	password = request.POST['password']
+	pw_hash = bcrypt.hashpw(password.encode, bcrypt.gensalt()).decode()
+	newuser = User.objects.create(
+		first_name = request.POST['first_name'],
+		last_name = request.POST['last_name'],
+		email = request.POST['email'],
+		password = pw_hash,
+		)
+	request.session['user_id'] = newuser.id
+	return redirect ('/userhome')
+
+#this is users feed. Will have feed, options to edit profile, post, and etc. 
+def userhome(request):
+	#if 'user_id' not in request.session:
+	#	return redirect('/')
+	#user = User.objects.get(id=request.session['user_id'])
+	#context = {
+	#    'user': user
+	#}
+	return render(request, 'userhome.html')#, context)
